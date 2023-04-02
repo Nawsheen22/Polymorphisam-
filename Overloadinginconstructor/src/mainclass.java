7ba5abd01ede17d0017475e932badcc5b08be9d1
Nawsheen Salsabeel <nawsheensalsabeel18@gmail.com>
2023-04-02 22:52:01 +0600

    Constructor initialized objects Constructors can print Constructors uses class name same constructors with different parameters

---
+++ Overloadinginconstructor/src/mainclass.java
@@ -0,0 +1,11 @@
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		
+		Candy c = new Candy ();
+		Candy c1 = new Candy ("Sneaker","USA","dark Chocolate");
+	    Candy c2 = new Candy ("Dark",100);
+	}
+
+}
